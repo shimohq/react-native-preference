c167948bbe65757bab74d1c2c4588be31f24b4c1
jiang947 <jiang947@163.com>
2019-12-31 11:23:31 +0800

    Preference可以在原生使用

--- android/src/main/java/im/shimo/react/preference/PreferenceModule.java
+++ android/src/main/java/im/shimo/react/preference/PreferenceModule.java
@@ -1,17 +1,9 @@
 package im.shimo.react.preference;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-
-import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.ReadableMap;
-import com.facebook.react.bridge.ReadableMapKeySetIterator;
-import com.facebook.react.bridge.WritableMap;
-import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -19,12 +11,10 @@ import java.util.Map;
 
 public class PreferenceModule extends ReactContextBaseJavaModule {
 
-    final private String mPreferenceKey = "data";
-    private SharedPreferences mSharedPreferences;
+    private final String mPreferenceKey = "data";
 
     public PreferenceModule(ReactApplicationContext context) {
         super(context);
-        mSharedPreferences = getReactApplicationContext().getSharedPreferences("react-native-preference", Context.MODE_PRIVATE);
     }
 
     public String getName() {
@@ -33,17 +23,13 @@ public class PreferenceModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void set(String data, Promise promise) {
-        SharedPreferences.Editor editor = getEditor();
-        editor.putString(mPreferenceKey, data);
-        editor.apply();
+        SharedPreferencesDelegate.getInstance().set(mPreferenceKey,data);
         promise.resolve(getPreferences());
     }
 
     @ReactMethod
     public void clear(Promise promise) {
-        SharedPreferences.Editor editor = getEditor();
-        editor.remove(mPreferenceKey);
-        editor.apply();
+        SharedPreferencesDelegate.getInstance().remove(mPreferenceKey);
         promise.resolve(getPreferences());
     }
 
@@ -55,16 +41,16 @@ public class PreferenceModule extends ReactContextBaseJavaModule {
     }
 
     private String getPreferences() {
-        return mSharedPreferences.getString(mPreferenceKey, "{}");
+        return SharedPreferencesDelegate.getInstance().get(mPreferenceKey, "{}");
     }
 
-    private SharedPreferences.Editor getEditor() {
-        return mSharedPreferences.edit();
-    }
+
 
     @Override
     public void onCatalystInstanceDestroy() {
         //mSharedPreferences.unregisterOnSharedPreferenceChangeListener(mListener);
     }
+
+
 
 }
---
+++ android/src/main/java/im/shimo/react/preference/SharedPreferencesDelegate.java
@@ -0,0 +1,74 @@
+package im.shimo.react.preference;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by jiang on 2019-12-26
+ */
+
+public class SharedPreferencesDelegate {
+
+    protected static volatile SharedPreferencesDelegate INSTANCE;
+
+    public static SharedPreferencesDelegate getInstance() {
+        return INSTANCE;
+    }
+
+    protected SharedPreferences mSharedPreferences;
+
+    protected SharedPreferencesDelegate(Context context) {
+        mSharedPreferences = context.getApplicationContext()
+                .getSharedPreferences("react-native-preference", Context.MODE_PRIVATE);
+    }
+
+    private SharedPreferences.Editor getEditor() {
+        return mSharedPreferences.edit();
+    }
+
+
+    public void set(String key, String data) {
+        SharedPreferences.Editor editor = getEditor();
+        editor.putString(key, data);
+        editor.apply();
+    }
+
+    public String get(String key, String defValue) {
+        return mSharedPreferences.getString(key, defValue);
+    }
+
+
+    public void remove(String key) {
+        SharedPreferences.Editor editor = getEditor();
+        editor.remove(key);
+        editor.apply();
+    }
+
+    public void register(SharedPreferences.OnSharedPreferenceChangeListener listener) {
+        mSharedPreferences.registerOnSharedPreferenceChangeListener(listener);
+    }
+
+    public void unregister(SharedPreferences.OnSharedPreferenceChangeListener listener) {
+        mSharedPreferences.unregisterOnSharedPreferenceChangeListener(listener);
+    }
+
+    public static void createInstance(Context context) {
+        if (INSTANCE == null) {
+            synchronized (SharedPreferencesDelegate.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new SharedPreferencesDelegate(context);
+                }
+            }
+        }
+    }
+
+
+    public boolean getBoolean(String key, boolean defValue) {
+        return mSharedPreferences.getBoolean(key, defValue);
+    }
+
+    public void putBoolean(String key, boolean value) {
+        getEditor().putBoolean(key, value);
+    }
+}
+
