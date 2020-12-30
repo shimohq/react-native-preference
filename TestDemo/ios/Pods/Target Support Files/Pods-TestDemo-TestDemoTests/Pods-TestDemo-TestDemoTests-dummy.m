ef4b8761de934d47bfe6e22c1ffa02f619e5ec01
谢天宸 <xietianchen@shimo.im>
2020-12-30 14:40:53 +0800

    chore: add testdemo

---
+++ TestDemo/ios/Pods/Target Support Files/Pods-TestDemo-TestDemoTests/Pods-TestDemo-TestDemoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestDemo_TestDemoTests : NSObject
+@end
+@implementation PodsDummy_Pods_TestDemo_TestDemoTests
+@end
--- ios/RNPreferenceManager.h
+++ ios/RNPreferenceManager.h
@@ -1,6 +1,7 @@
 #import <React/RCTBridgeModule.h>
 #import <React/RCTEventEmitter.h>
 
+
 @interface RNPreferenceManager : RCTEventEmitter <RCTBridgeModule>
 + (id)getPreference:(NSString *)key;
 + (NSString *)getPreferences;
