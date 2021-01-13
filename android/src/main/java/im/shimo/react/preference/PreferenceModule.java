25c9ca4a2c4a5d94bc8e5155898282bd6727c506
谢天宸 <xietianchen@shimo.im>
2021-01-13 20:58:43 +0800

    wip: 调整读写接口

--- android/src/main/java/im/shimo/react/preference/PreferenceModule.java
+++ android/src/main/java/im/shimo/react/preference/PreferenceModule.java
@@ -1,7 +1,5 @@
 package im.shimo.react.preference;
 
-import android.util.Log;
-
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -14,8 +12,6 @@ import java.util.Map;
 
 public class PreferenceModule extends ReactContextBaseJavaModule {
 
-    private final String mPreferenceKey = "data";
-
     public PreferenceModule(ReactApplicationContext context) {
         super(context);
     }
@@ -26,19 +22,20 @@ public class PreferenceModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void set(String data, Promise promise) {
-        getDelegate().set(mPreferenceKey,data);
-        promise.resolve(getPreferences());
+        getDelegate().setPreferenceData(data);
+        promise.resolve(getDelegate().getAllPreferences());
     }
 
     @ReactMethod
     public void clear(Promise promise) {
-        getDelegate().remove(mPreferenceKey);
-        promise.resolve(getPreferences());
+        getDelegate().remove();
+        promise.resolve(getDelegate().getAllPreferences());
     }
 
     @ReactMethod
     public void getPreferenceForKey(String key, Promise promise) {
-        //getDelegate();
+        getDelegate().getPreferenceValueForKey(key);
+        promise.resolve(getDelegate().getAllPreferences());
     }
 
     @ReactMethod
@@ -51,13 +48,13 @@ public class PreferenceModule extends ReactContextBaseJavaModule {
     @Override
     public Map<String, Object> getConstants() {
         final Map<String, Object> constants = new HashMap<>();
-        constants.put("InitialPreferences", getPreferences());
+        constants.put("InitialPreferences", getDelegate().getAllPreferences());
         return constants;
     }
 
-    private String getPreferences() {
-        return getDelegate().get(mPreferenceKey, "{}");
-    }
+
+
+
 
     private SharedPreferencesDelegate getDelegate(){
         if (SharedPreferencesDelegate.getInstance()==null){
@@ -65,8 +62,6 @@ public class PreferenceModule extends ReactContextBaseJavaModule {
         }
         return SharedPreferencesDelegate.getInstance();
     }
-
-
 
     @Override
     public void onCatalystInstanceDestroy() {
--- android/src/main/java/im/shimo/react/preference/SharedPreferencesDelegate.java
+++ android/src/main/java/im/shimo/react/preference/SharedPreferencesDelegate.java
@@ -3,15 +3,23 @@ package im.shimo.react.preference;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by jiang on 2019-12-26
  */
 
+
+
 public class SharedPreferencesDelegate {
 
     protected static volatile SharedPreferencesDelegate INSTANCE;
+
+    private final String mPreferenceKey = "data";
 
     public static SharedPreferencesDelegate getInstance() {
         return INSTANCE;
@@ -19,7 +27,9 @@ public class SharedPreferencesDelegate {
 
     protected SharedPreferences mSharedPreferences;
 
-    public static ArrayList<Object> whiteList;
+    public ArrayList<Object> whiteList;
+    public Map<String, String> singlePreference;
+
 
     protected SharedPreferencesDelegate(Context context) {
         mSharedPreferences = context.getApplicationContext()
@@ -30,23 +40,37 @@ public class SharedPreferencesDelegate {
         return mSharedPreferences.edit();
     }
 
+    public String getAllPreferences() {
+        return mSharedPreferences.getString(mPreferenceKey, "{}");
+    }
 
-    public void set(String key, String data) {
+    public String getPreferenceValueForKey(String key) {
+        return INSTANCE.singlePreference.get(key);
+    }
+
+    public void setPreferenceItem(String value, String key) {
+        INSTANCE.singlePreference.put(key,value);
+//        NSMutableDictionary *tmpMap = [self.singlePerference mutableCopy];
+//        [tmpMap setObject:value forKey:key];
+//        [self setPreferenceData:RCTJSONStringify(tmpMap, nil)];
+    }
+
+    public void setPreferenceData(String data) {
+
+
         SharedPreferences.Editor editor = getEditor();
-        editor.putString(key, data);
+        editor.putString(mPreferenceKey, data);
         editor.apply();
     }
 
-    public String get(String key, String defValue) {
-        return mSharedPreferences.getString(key, defValue);
-    }
 
-
-    public void remove(String key) {
+    public void remove() {
         SharedPreferences.Editor editor = getEditor();
-        editor.remove(key);
+        editor.remove(mPreferenceKey);
         editor.apply();
     }
+
+
 
     public void register(SharedPreferences.OnSharedPreferenceChangeListener listener) {
         mSharedPreferences.registerOnSharedPreferenceChangeListener(listener);
@@ -61,11 +85,11 @@ public class SharedPreferencesDelegate {
             synchronized (SharedPreferencesDelegate.class) {
                 if (INSTANCE == null) {
                     INSTANCE = new SharedPreferencesDelegate(context);
+                    //INSTANCE.singlePerference = RCTJSONParse([RNPreferenceSingleton getAllPreferences], nil);
                 }
             }
         }
     }
-
 
     public boolean getBoolean(String key, boolean defValue) {
         return mSharedPreferences.getBoolean(key, defValue);
