42bc57a776ae0451acfa09991948c6fbd46861f1
谢天宸 <xietianchen@shimo.im>
2021-01-14 19:12:55 +0800

    feat: 安卓走通

--- android/src/main/java/im/shimo/react/preference/PreferenceModule.java
+++ android/src/main/java/im/shimo/react/preference/PreferenceModule.java
@@ -1,19 +1,33 @@
 package im.shimo.react.preference;
 
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.google.gson.Gson;
 
 import java.util.HashMap;
 import java.util.Map;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import javax.annotation.Nullable;
 
 
 public class PreferenceModule extends ReactContextBaseJavaModule {
 
+    private BroadcastReceiver sharePreferenceReceiver;
+    private Gson gson = new Gson();
+
     public PreferenceModule(ReactApplicationContext context) {
         super(context);
+        registerSharePreferenceReceiver();
     }
 
     public String getName() {
@@ -43,8 +57,6 @@ public class PreferenceModule extends ReactContextBaseJavaModule {
         getDelegate().whiteList = whiteList.toArrayList();
     }
 
-
-
     @Override
     public Map<String, Object> getConstants() {
         final Map<String, Object> constants = new HashMap<>();
@@ -52,9 +64,10 @@ public class PreferenceModule extends ReactContextBaseJavaModule {
         return constants;
     }
 
-
-
-
+    @Override
+    public void onCatalystInstanceDestroy() {
+        getReactApplicationContext().unregisterReceiver(sharePreferenceReceiver);
+    }
 
     private SharedPreferencesDelegate getDelegate(){
         if (SharedPreferencesDelegate.getInstance()==null){
@@ -63,11 +76,33 @@ public class PreferenceModule extends ReactContextBaseJavaModule {
         return SharedPreferencesDelegate.getInstance();
     }
 
-    @Override
-    public void onCatalystInstanceDestroy() {
-        //mSharedPreferences.unregisterOnSharedPreferenceChangeListener(mListener);
+    private void registerSharePreferenceReceiver() {
+        sharePreferenceReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String msg = intent.getStringExtra("MSG");
+                HashMap<String,String> msgMap = gson.fromJson(msg, HashMap.class);
+                WritableMap map = Arguments.createMap();
+                for (Map.Entry<String, String> entry : msgMap.entrySet()) {
+                    map.putString(entry.getKey(),entry.getValue());
+                }
+
+                sendEvent(
+                    getReactApplicationContext(),
+                    SharedPreferencesDelegate.kSHMPreferenceChangedNotification,
+                    map
+                    );
+            }
+        };
+        IntentFilter intentFilter = new IntentFilter(SharedPreferencesDelegate.kSHMPreferenceChangedNotification);
+        getReactApplicationContext().registerReceiver(sharePreferenceReceiver, intentFilter);
     }
 
-
-
+    private void sendEvent(ReactContext reactContext,
+                           String eventName,
+                           @Nullable WritableMap params) {
+        reactContext
+            .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+            .emit(eventName, params);
+    }
 }
--- android/src/main/java/im/shimo/react/preference/SharedPreferencesDelegate.java
+++ android/src/main/java/im/shimo/react/preference/SharedPreferencesDelegate.java
@@ -1,10 +1,11 @@
 package im.shimo.react.preference;
 
+import com.google.gson.Gson;
+
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
-
-import org.json.JSONObject;
-
+import android.util.Log;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,11 +14,11 @@ import java.util.Map;
  * Created by jiang on 2019-12-26
  */
 
-
-
 public class SharedPreferencesDelegate {
 
     protected static volatile SharedPreferencesDelegate INSTANCE;
+
+    public static String kSHMPreferenceChangedNotification = "SHMPreferenceWhiteListChanged";
 
     private final String mPreferenceKey = "data";
 
@@ -27,9 +28,12 @@ public class SharedPreferencesDelegate {
 
     protected SharedPreferences mSharedPreferences;
 
+    private Gson gson = new Gson();
+    private static final String TAG = SharedPreferencesDelegate.class.getSimpleName();
+    private Context mContext;
+
     public ArrayList<Object> whiteList;
     public Map<String, String> singlePreference;
-
 
     protected SharedPreferencesDelegate(Context context) {
         mSharedPreferences = context.getApplicationContext()
@@ -49,18 +53,63 @@ public class SharedPreferencesDelegate {
     }
 
     public void setPreferenceItem(String value, String key) {
-        INSTANCE.singlePreference.put(key,value);
-//        NSMutableDictionary *tmpMap = [self.singlePerference mutableCopy];
-//        [tmpMap setObject:value forKey:key];
-//        [self setPreferenceData:RCTJSONStringify(tmpMap, nil)];
+        HashMap<String, String> tmpMap = new HashMap<String, String>();
+        tmpMap.putAll(INSTANCE.singlePreference);
+        tmpMap.put(key,value);
+        setPreferenceData(gson.toJson(tmpMap,HashMap.class));
     }
 
     public void setPreferenceData(String data) {
+        Map<String, String> obj = gson.fromJson(data, Map.class);
+        if (obj == null) {
+            Log.w(TAG, "err: setPreferenceData - wrong data type!");
+            return;
+        }
 
+        if (INSTANCE.whiteList.size() == 0) {
+            Log.w(TAG, "RNPreference - white list is null !");
+        }
 
+        // Diff
+        Map<String, String> mapNew = obj;
+        Map<String, String> mapOld = INSTANCE.singlePreference;
+
+        // set data
+        INSTANCE.singlePreference = obj;
         SharedPreferences.Editor editor = getEditor();
         editor.putString(mPreferenceKey, data);
         editor.apply();
+
+        // 1. map is equal
+        if (!mapNew.equals(mapOld)) {
+            if (data.equals("{}") && mapOld != null) {
+                Intent intent = new Intent(kSHMPreferenceChangedNotification);
+                intent.putExtra("MSG", "{}");
+                INSTANCE.mContext.sendBroadcast(intent);
+
+                return;
+            }
+
+            //2. whitelist
+            for (int i = 0; i < INSTANCE.whiteList.size(); i ++) {
+                String key = (String)INSTANCE.whiteList.get(i);
+                String strNew = mapNew.get(key);
+                String strOld = mapOld.get(key);
+                if (strNew == null || strNew.length() == 0) return;
+
+                if (!strNew.equals(strOld)) {
+                    //3. data changed, send msg to JS
+                    Map<String, String> item = new HashMap<>();
+                    item.put(key,strNew);
+                    Log.d(TAG, String.format( "RNPreference (key) {%s} , Changed : {%s}",key,strNew));
+                    Intent intent = new Intent(kSHMPreferenceChangedNotification);
+                    Map<String, String> msgMap = new HashMap<String, String>();
+                    msgMap.put(key,strNew);
+                    intent.putExtra("MSG", gson.toJson(msgMap,Map.class));
+                    INSTANCE.mContext.sendBroadcast(intent);
+                }
+            }
+        }
     }
 
 
@@ -85,7 +134,8 @@ public class SharedPreferencesDelegate {
             synchronized (SharedPreferencesDelegate.class) {
                 if (INSTANCE == null) {
                     INSTANCE = new SharedPreferencesDelegate(context);
-                    //INSTANCE.singlePerference = RCTJSONParse([RNPreferenceSingleton getAllPreferences], nil);
+                    INSTANCE.singlePreference = INSTANCE.gson.fromJson(INSTANCE.getAllPreferences(), Map.class);
+                    INSTANCE.mContext = context;
                 }
             }
         }
